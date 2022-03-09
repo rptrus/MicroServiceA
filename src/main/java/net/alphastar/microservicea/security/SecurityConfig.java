16fc108bf4b41da30cf8ecc4bf4f353251c57a3f
Rohan Truscott <r.truscott@gmail.com>
2022-03-10 00:35:48 +1100

    Use session management to fix cached credentials issue Add an example unsecured API

--- src/main/java/net/alphastar/microservicea/security/SecurityConfig.java
+++ src/main/java/net/alphastar/microservicea/security/SecurityConfig.java
@@ -1,14 +1,13 @@
 package net.alphastar.microservicea.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -17,10 +16,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private UserDetailsService userDetailsService;
-
-    @Autowired
-    private BCryptPasswordEncoder encodedPass;
-
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -32,7 +27,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         //http.csrf().disable().authorizeRequests().antMatchers("/api/**").authenticated().and().httpBasic();
         http.httpBasic().and().authorizeRequests()
                 .antMatchers("/api/**").hasRole("ADMIN")
-                .and().csrf().disable();
+                .antMatchers("/api/goodbye").permitAll()
+                .and().csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 }
 
--- src/main/java/net/alphastar/microservicea/service/HelloService.java
+++ src/main/java/net/alphastar/microservicea/service/HelloService.java
@@ -1,9 +1,6 @@
 package net.alphastar.microservicea.service;
 
-import net.alphastar.microservicea.DAO.UsersDao;
 import net.alphastar.microservicea.DTO.HelloObject;
-import net.alphastar.microservicea.entity.Users;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,20 +13,21 @@ import java.util.Date;
 @RequestMapping("/api")
 public class HelloService {
 
-    @Autowired
-    private UsersDao usersDao;
-
     @GetMapping(path = "/hello")
     @PreAuthorize("hasRole('ADMIN')")
     public HelloObject sayHello() {
-
-        for (Users user: usersDao.findAll()) {
-            System.out.println(user.getUsername());
-        }
-
         HelloObject helloObject = new HelloObject();
         helloObject.setData(new SimpleDateFormat("yyyyMMdd HHmmss").format(new Date()));
         helloObject.setNumber(1);
         return helloObject;
     }
+
+    @GetMapping(path = "/goodbye")
+    public HelloObject sayGoodBye() {
+        HelloObject helloObject = new HelloObject();
+        helloObject.setData(new SimpleDateFormat("yyyyMMdd HHmmss").format(new Date()));
+        helloObject.setNumber(2);
+        return helloObject;
+    }
+
 }
